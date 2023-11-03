3367fd4609eaf5b0a00e3564b5a6d54041447180
finmcf <92210179+finmcf@users.noreply.github.com>
2023-11-03 05:14:22 +0000

    updates

--- components/ReservationSearch/LocationInput.tsx
+++ components/ReservationSearch/LocationInput.tsx
@@ -5,11 +5,10 @@ const LocationInput = ({ onLocationSelect }) => {
   const [suggestions, setSuggestions] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // Function to fetch autocomplete suggestions from your backend
   const fetchSuggestions = async (keyword) => {
     setLoading(true);
     try {
-      const clientId = process.env.NEXT_PUBLIC_CLIENT_ID; // Ensure these are securely managed in your environment
+      const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
       const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
 
       const response = await fetch(
@@ -19,48 +18,56 @@ const LocationInput = ({ onLocationSelect }) => {
         {
           method: "GET",
           headers: {
-            // Include other headers your backend might require
-            ClientId: clientId, // Send the client ID if required by your backend
-            ClientSecret: clientSecret, // Send the client secret if required by your backend
+            ClientId: clientId,
+            ClientSecret: clientSecret,
           },
         }
       );
 
       if (!response.ok) {
-        throw new Error("Failed to fetch suggestions");
+        throw new Error(
+          `Failed to fetch suggestions, status: ${response.status}`
+        );
       }
 
       const data = await response.json();
-      // Assuming your API returns an array under a data property
-      setSuggestions(data.data);
+      if (Array.isArray(data)) {
+        setSuggestions(data);
+      } else {
+        console.error("Response data is not in expected format:", data);
+        setSuggestions([]);
+      }
     } catch (error) {
       console.error("Error fetching suggestions:", error);
+      setSuggestions([]);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    if (input.length >= 3) {
-      const timerId = setTimeout(() => fetchSuggestions(input), 500);
-      return () => clearTimeout(timerId);
-    } else {
-      setSuggestions([]);
-    }
+    const debounceFetch = setTimeout(() => {
+      if (input.length >= 3) {
+        fetchSuggestions(input);
+      } else {
+        setSuggestions([]);
+      }
+    }, 500);
+    return () => clearTimeout(debounceFetch);
   }, [input]);
 
   const handleSuggestionClick = (suggestion) => {
     setInput(suggestion.name);
     setSuggestions([]);
-    onLocationSelect(suggestion); // Pass the selected suggestion up to the parent component
+    onLocationSelect(suggestion);
   };
 
   return (
     <div className="flex-1 relative">
       <input
         type="text"
-        placeholder="Location"
-        className="w-full px-2 py-1 border border-gray-300 rounded text-center"
+        placeholder="Search location"
+        className="w-full px-2 py-1 border border-gray-300 rounded"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         autoComplete="off"
@@ -70,15 +77,15 @@ const LocationInput = ({ onLocationSelect }) => {
           Loading...
         </div>
       )}
-      {suggestions.length > 0 && (
+      {suggestions && suggestions.length > 0 && (
         <ul className="absolute left-0 right-0 max-h-60 overflow-auto border border-gray-300 bg-white z-10">
-          {suggestions.map((suggestion, index) => (
+          {suggestions.map((suggestion) => (
             <li
-              key={suggestion.id} // use unique id for key if available
+              key={suggestion.id}
               className="px-2 py-1 hover:bg-gray-100 cursor-pointer"
               onClick={() => handleSuggestionClick(suggestion)}
             >
-              {suggestion.name} ({suggestion.address.cityName})
+              {suggestion.name} ({suggestion.address?.cityName})
             </li>
           ))}
         </ul>
