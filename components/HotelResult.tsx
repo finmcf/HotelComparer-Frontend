00eef3ea69638acf3a51dcf6c3fa3338885f8d8c
finmcf <92210179+finmcf@users.noreply.github.com>
2023-11-18 16:20:29 +0000

    updates

--- components/HotelResult.tsx
+++ components/HotelResult.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const HotelResult = () => {
   return (
-    <div className="w-[780px] h-[167px] left-[54px] top-[95px] absolute bg-neutral-500 rounded-[20px]">
+    <div className="w-[95%] h-[167px] mx-auto mt-[50px] bg-neutral-500 rounded-[20px] relative">
       <div className="w-[136px] h-[136px] left-[17px] top-[15px] absolute bg-red-600 rounded-[20px]" />
-      <div className="px-[11px] py-[9px] left-[615px] top-[118px] absolute bg-green-400 rounded-[20px] justify-center items-center inline-flex">
+      <div className="px-[11px] py-[9px] right-[20px] top-[118px] absolute bg-green-400 rounded-[20px] justify-center items-center inline-flex">
         <div className="text-black text-xs font-normal font-['Inter']">
           View Booking Options
         </div>
       </div>
-      <div className="pl-[73px] pr-2.5 py-1 left-[626px] top-[73px] absolute bg-neutral-400 flex-col justify-center items-end inline-flex">
+      <div className="pl-[73px] pr-2.5 py-1 right-[20px] top-[73px] absolute bg-neutral-400 flex-col justify-center items-end inline-flex">
         <div className="text-black text-base font-normal font-['Inter']">
           $ Price
         </div>
--- components/HotelSearchResult.tsx
+++ components/HotelSearchResult.tsx
@@ -3,13 +3,13 @@ import HotelResult from "./HotelResult"; // Import the HotelResult component
 
 const HotelSearchResults = () => {
   return (
-    <div className="flex-1 h-[690px] relative bg-stone-400">
-      <div className="w-[441px] pl-[17px] pr-[283px] pt-3 pb-[11px] left-[54px] top-[32px] absolute bg-white justify-start items-center inline-flex">
+    <div className="flex-1 h-[690px] flex justify-center items-start bg-stone-400">
+      <div className="w-[441px] pl-[17px] pr-[283px] pt-3 pb-[11px] absolute top-[32px] bg-white justify-start items-center inline-flex">
         <div className="text-black text-xs font-normal font-['Inter']">
           London, December 9-12
         </div>
       </div>
-      <div className="w-[99px] h-[38px] left-[735px] top-[32px] absolute bg-zinc-300 rounded-[20px]" />
+      <div className="w-[99px] h-[38px] right-[54px] top-[32px] absolute bg-zinc-300 rounded-[20px]" />
       <HotelResult />
     </div>
   );
