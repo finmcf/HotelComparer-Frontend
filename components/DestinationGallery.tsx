a396b93f8d240e370ba0ed18ee1c34c9b83369fc
finmcf <92210179+finmcf@users.noreply.github.com>
2023-12-03 18:45:50 +0000

    updates

--- components/DestinationGallery.tsx
+++ components/DestinationGallery.tsx
@@ -1,63 +1,84 @@
 import React, { useState, useEffect } from "react";
 import DestinationImage from "./DestinationImage";
 import SplitDestinationImage from "./SplitDestinationImage";
+import imageData from "../data/split_city_images.json"; // Importing the JSON data
+import { useImageSearch } from "../utilities/handleImageSearch"; // Adjust the path as necessary
+
+interface ImageData {
+  src: string;
+  latitude: string;
+  longitude: string;
+  city: string;
+}
+
+interface SplitDestinationImageProps {
+  topImage: ImageData;
+  bottomImage: ImageData;
+  onImageClick: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void;
+}
 
 const DestinationGallery: React.FC = () => {
-  const [isSplitFirst, setIsSplitFirst] = useState(false);
+  const [isSplitFirst, setIsSplitFirst] = useState<boolean>(false);
+  const [imagePairs, setImagePairs] = useState<Array<[ImageData, ImageData]>>(
+    []
+  );
+
+  const imageSearch = useImageSearch(); // Using the useImageSearch hook
 
   useEffect(() => {
-    // Randomize the order only after component mounts
+    const shuffledImages: ImageData[] = [...imageData].sort(
+      () => 0.5 - Math.random()
+    );
+    const pairs: Array<[ImageData, ImageData]> = [];
+    for (let i = 0; i < shuffledImages.length; i += 2) {
+      pairs.push([shuffledImages[i], shuffledImages[i + 1]]);
+    }
+
+    setImagePairs(pairs);
     setIsSplitFirst(Math.random() < 0.5);
   }, []);
 
   const renderImages = () => {
-    return (
-      <>
-        {isSplitFirst ? (
-          <>
-            <SplitDestinationImage
-              topSrc="https://via.placeholder.com/272x200"
-              bottomSrc="https://via.placeholder.com/272x200"
-              alt="Travel Destination 2"
-            />
-            <DestinationImage
-              src="https://via.placeholder.com/272x381"
-              alt="Travel Destination 1"
-            />
-            <SplitDestinationImage
-              topSrc="https://via.placeholder.com/272x200"
-              bottomSrc="https://via.placeholder.com/272x200"
-              alt="Travel Destination 4"
-            />
-            <DestinationImage
-              src="https://via.placeholder.com/272x381"
-              alt="Travel Destination 3"
-            />
-          </>
-        ) : (
-          <>
-            <DestinationImage
-              src="https://via.placeholder.com/272x381"
-              alt="Travel Destination 1"
-            />
-            <SplitDestinationImage
-              topSrc="https://via.placeholder.com/272x200"
-              bottomSrc="https://via.placeholder.com/272x200"
-              alt="Travel Destination 2"
-            />
-            <DestinationImage
-              src="https://via.placeholder.com/272x381"
-              alt="Travel Destination 3"
-            />
-            <SplitDestinationImage
-              topSrc="https://via.placeholder.com/272x200"
-              bottomSrc="https://via.placeholder.com/272x200"
-              alt="Travel Destination 4"
-            />
-          </>
-        )}
-      </>
-    );
+    const components: JSX.Element[] = [];
+
+    for (let i = 0; i < imagePairs.length; i++) {
+      const pair = imagePairs[i];
+
+      // Alternate starting with SplitDestinationImage or DestinationImage
+      if (isSplitFirst) {
+        components.push(
+          <SplitDestinationImage
+            key={`split-${i}`}
+            topImage={pair[0]}
+            bottomImage={pair[1]}
+            onImageClick={(event) => imageSearch(event)}
+          />
+        );
+      }
+
+      // Add DestinationImage in between or at the end
+      components.push(
+        <DestinationImage
+          key={`dest-${i}`}
+          src="https://via.placeholder.com/272x381"
+          alt={`Travel Destination ${i + 1}`}
+        />
+      );
+
+      // If we started with DestinationImage, add SplitDestinationImage after it
+      if (!isSplitFirst) {
+        components.push(
+          <SplitDestinationImage
+            key={`split-${i}`}
+            topImage={pair[0]}
+            bottomImage={pair[1]}
+            onImageClick={(event) => imageSearch(event)}
+          />
+        );
+      }
+    }
+
+    return <>{components}</>;
   };
 
   return (
--- components/SplitDestinationImage.tsx
+++ components/SplitDestinationImage.tsx
@@ -1,40 +1,53 @@
-// Updated interface
-interface SplitDestinationImageProps {
-  topSrc: string;
-  bottomSrc: string;
-  alt: string;
+import React from "react";
+
+interface ImageData {
+  src: string;
+  latitude: string;
+  longitude: string;
+  city: string;
 }
 
-// Updated component
+interface SplitDestinationImageProps {
+  topImage: ImageData;
+  bottomImage: ImageData;
+  onImageClick: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void;
+}
+
 const SplitDestinationImage: React.FC<SplitDestinationImageProps> = ({
-  topSrc,
-  bottomSrc,
-  alt,
+  topImage,
+  bottomImage,
+  onImageClick,
 }) => {
   const halfHeight = (381 - 10) / 2;
 
   return (
-    <div className="flex flex-col items-center w-[272.25px]">
-      <div className="h-[185.5px] overflow-hidden rounded-xl">
+    <div className="flex flex-col items-center w-[272.25px] ">
+      <div className="h-[185.5px] overflow-hidden rounded-xl cursor-pointer">
         <img
-          src={topSrc}
-          alt={`${alt} - Top Half`}
+          src={topImage.src}
+          alt={`${topImage.city} - Top`}
           style={{
             width: "272.25px",
             height: `${halfHeight}px`,
             display: "block",
           }}
+          data-latitude={topImage.latitude}
+          data-longitude={topImage.longitude}
+          onClick={onImageClick}
         />
       </div>
-      <div className="h-[185.5px] overflow-hidden rounded-xl mt-2.5">
+      <div className="h-[185.5px] overflow-hidden rounded-xl mt-2.5 cursor-pointer">
         <img
-          src={bottomSrc}
-          alt={`${alt} - Bottom Half`}
+          src={bottomImage.src}
+          alt={`${bottomImage.city} - Bottom`}
           style={{
             width: "272.25px",
             height: `${halfHeight}px`,
             display: "block",
           }}
+          data-latitude={bottomImage.latitude}
+          data-longitude={bottomImage.longitude}
+          onClick={onImageClick}
         />
       </div>
     </div>
