a06fa3204d293f885a478b094430d460554feb8c
finmcf <92210179+finmcf@users.noreply.github.com>
2023-12-04 04:22:29 +0000

    updates

--- components/DestinationGallery.tsx
+++ components/DestinationGallery.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import DestinationImage from "./DestinationImage";
 import SplitDestinationImage from "./SplitDestinationImage";
-import imageData from "../data/split_city_images.json"; // Importing the JSON data
-import { useImageSearch } from "../utilities/handleImageSearch"; // Adjust the path as necessary
+import splitImageData from "../data/split_city_images.json";
+import cityImageData from "../data/city_images.json";
+import { useImageSearch } from "../utilities/handleImageSearch";
 
 interface ImageData {
   src: string;
@@ -11,67 +12,52 @@ interface ImageData {
   city: string;
 }
 
-interface SplitDestinationImageProps {
-  topImage: ImageData;
-  bottomImage: ImageData;
-  onImageClick: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void;
-}
-
 const DestinationGallery: React.FC = () => {
   const [isSplitFirst, setIsSplitFirst] = useState<boolean>(false);
-  const [imagePairs, setImagePairs] = useState<Array<[ImageData, ImageData]>>(
-    []
-  );
+  const [cityImages, setCityImages] = useState<ImageData[]>([]);
 
-  const imageSearch = useImageSearch(); // Using the useImageSearch hook
+  const imageSearch = useImageSearch();
 
   useEffect(() => {
-    const shuffledImages: ImageData[] = [...imageData].sort(
+    const shuffledCityImages: ImageData[] = [...cityImageData].sort(
       () => 0.5 - Math.random()
     );
-    const pairs: Array<[ImageData, ImageData]> = [];
-    for (let i = 0; i < shuffledImages.length; i += 2) {
-      pairs.push([shuffledImages[i], shuffledImages[i + 1]]);
-    }
-
-    setImagePairs(pairs);
+    setCityImages(shuffledCityImages.slice(0, 2));
     setIsSplitFirst(Math.random() < 0.5);
   }, []);
 
   const renderImages = () => {
     const components: JSX.Element[] = [];
 
-    for (let i = 0; i < imagePairs.length; i++) {
-      const pair = imagePairs[i];
-
-      // Alternate starting with SplitDestinationImage or DestinationImage
+    for (let i = 0; i < 2; i++) {
       if (isSplitFirst) {
         components.push(
           <SplitDestinationImage
             key={`split-${i}`}
-            topImage={pair[0]}
-            bottomImage={pair[1]}
+            topImage={splitImageData[i * 2]}
+            bottomImage={splitImageData[i * 2 + 1]}
             onImageClick={(event) => imageSearch(event)}
           />
         );
       }
 
-      // Add DestinationImage in between or at the end
-      components.push(
-        <DestinationImage
-          key={`dest-${i}`}
-          src="https://via.placeholder.com/272x381"
-          alt={`Travel Destination ${i + 1}`}
-        />
-      );
+      // Ensure cityImage is defined before rendering DestinationImage
+      if (cityImages[i]) {
+        components.push(
+          <DestinationImage
+            key={`dest-${i}`}
+            image={cityImages[i]}
+            onImageClick={(event) => imageSearch(event)}
+          />
+        );
+      }
 
-      // If we started with DestinationImage, add SplitDestinationImage after it
       if (!isSplitFirst) {
         components.push(
           <SplitDestinationImage
             key={`split-${i}`}
-            topImage={pair[0]}
-            bottomImage={pair[1]}
+            topImage={splitImageData[i * 2]}
+            bottomImage={splitImageData[i * 2 + 1]}
             onImageClick={(event) => imageSearch(event)}
           />
         );
--- components/DestinationImage.tsx
+++ components/DestinationImage.tsx
@@ -1,19 +1,30 @@
-// DestinationImage.tsx
 import React from "react";
 
-interface DestinationImageProps {
+interface ImageData {
   src: string;
-  alt: string;
+  latitude: string;
+  longitude: string;
+  city: string;
 }
 
-const DestinationImage: React.FC<DestinationImageProps> = ({ src, alt }) => {
+interface DestinationImageProps {
+  image: ImageData;
+  onImageClick: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void;
+}
+
+const DestinationImage: React.FC<DestinationImageProps> = ({
+  image,
+  onImageClick,
+}) => {
   return (
     <div className="flex items-center justify-center w-[272.25px] h-[381px]">
       <img
-        src={src}
-        alt={alt}
-        className="rounded-xl"
-        style={{ width: "272.25px", height: "381px" }}
+        src={image.src}
+        alt={`Travel Destination ${image.city}`}
+        data-latitude={image.latitude}
+        data-longitude={image.longitude}
+        onClick={onImageClick}
+        className="rounded-xl cursor-pointer w-[272.25px] h-[381px] object-cover"
       />
     </div>
   );
